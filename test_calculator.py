8fd6cc75598f991b2be4867da400a70639e2ddae
mnkap <mnkapezov@gmail.com>
2022-06-12 15:38:42 +0600

    Add configuration file for python calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+import unittest
+
+from calculator import multiply
+
+class TestSomething(unittest.TestCase):
+	def test_multiply(self):
+		self.assertEqual(6, multiply(2,3))
+
+if __name__ == '__main__':
+	unittest.main()
